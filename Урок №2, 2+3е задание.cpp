d75306f6df796a40c3f1dcca6f4db9ff693ad657
0danata <85079890+0danata@users.noreply.github.com>
2021-06-28 13:33:47 +0300

    Урок №2, 2+3е задание

---
+++ Урок №2, 2+3е задание.cpp
@@ -0,0 +1,17 @@
+﻿
+#include <iostream>
+
+using namespace std;
+
+  enum Igra { Player1, Player2, x, y };
+
+int main() 
+{
+    int Game[3][3]{ {'_','_','_'],{'_','_','_'],{'_','_','_'] };
+    Game [x][y] = 1;  
+    // Дальше я запутался 
+
+
+    
+    return 0;
+}
