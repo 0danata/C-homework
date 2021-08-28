825694c98dedcc4f09124f594fdead9cfc69e148
0danata <85079890+0danata@users.noreply.github.com>
2021-08-28 14:16:16 +0300

    Код проверки числа
    Написать функцию проверки, является ли число простым  В коде на языке С
    Написал на С++. Так как не разобрался как Eclipse указать путь до библиотек minGW

---
+++ homework_task2.cpp
@@ -0,0 +1,14 @@
+﻿
+#include <iostream> 
+#include <cstdlib>
+using namespace std;
+
+bool test(const int a) {
+    return(a % 1 == 0 && a % 2 == 0);
+}
+int main() {
+    int a;
+    cin >> a;
+    cout << (test(a) ? "True" : "False") << endl;
+    return 0;
+}
