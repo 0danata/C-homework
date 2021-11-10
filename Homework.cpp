31853902f2ea1cb21b5577b273eb7d2d327a33b4
0danata <85079890+0danata@users.noreply.github.com>
2021-11-10 21:29:31 +0300

    Функция проверки, является ли число простым
    - В коде на языке С++

---
+++ Homework.cpp
@@ -0,0 +1,29 @@
+﻿
+#include <iostream>
+
+using namespace std;
+
+bool Prime(int a) // Функция провыерки простого числа
+{
+    if (a < 2)
+        return false;
+
+    else
+    {
+        for (size_t i = 2; i * i <= a; i++)
+        {
+            if (a % i == 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+
+int main()
+{
+    cout << (Prime(197) ? "True" : "False");
+}
+ 
